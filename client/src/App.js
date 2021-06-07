e3b87636e566760bce8ea25efa9b68e55575fde7
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2021-06-07 08:44:21 -0400

    Frontend hamburger options changes

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,9 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import SettingsIcon from '@material-ui/icons/Settings';
+import MessageIcon from '@material-ui/icons/Message'
+import ContactsIcon from '@material-ui/icons/Contacts';
 
 const drawerWidth = 240;
 
@@ -130,18 +131,26 @@ export default function PersistentDrawerLeft() {
         </div>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Contacts'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon>{<ContactsIcon/>}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <List>
+        {['Messages'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{<MessageIcon/>}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          {['Settings'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon>{<SettingsIcon/>}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
